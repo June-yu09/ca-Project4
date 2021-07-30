81ad0d088df8156193a730801956615c32b76d32
June-yu09 <yuzatee@gmail.com>
2021-07-30 09:40:25 +0200

    heroku

--- server/s3.js
+++ server/s3.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
+const bucketName = require('./keys').AWS_BUCKET_NAME;
+const region = require('./keys').AWS_BUCKET_REGION;
+const accessKeyId = require('./keys').AWS_ACCESS_KEY;
+const secretAccessKey = require('./keys').AWS_SECRET_KEY;
+
 
 const fs = require('fs');
 const S3 = require('aws-sdk/clients/s3');
-
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 const s3 = new S3({
     region,
